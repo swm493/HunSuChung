217235ed94d6876ffc16f8f97b2b8d119812e6b0
jjong22 <jjong13971397@gmail.com>
2025-05-07 05:34:43 +0900

    feat: change logic of spike
    찔리면 씬을 재시작하는 것이 아니라, 처음 위치로 돌리는 것으로 변경

--- Assets/Scripts/TrapCollision.cs
+++ Assets/Scripts/TrapCollision.cs
@@ -1,14 +1,39 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class SpikeCollision : MonoBehaviour
 {
+    public Transform respawnPoint;
+
+    private Transform player;
+    private Vector3 initialPos;
+
+    void Start() // get player's transform
+    {
+        player = GameObject.FindGameObjectWithTag("Player")?.transform;
+
+        if (respawnPoint == null)
+            initialPos = player.position;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            // 예: 현재 씬을 다시 로드
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            RespawnPlayer();
+        }
+    }
+
+    void RespawnPlayer()
+    {
+        // 위치 이동
+        Vector3 targetPos = respawnPoint ? respawnPoint.position : initialPos;
+        player.position = targetPos;
+
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0f;
         }
     }
 }
