1633dbd10dcf45217f2e275910665103f64d8715
jjong22 <jjong13971397@gmail.com>
2025-05-19 19:15:22 +0900

    [Feat] Add Player Limit

--- Assets/Scripts/Object/PlayerController.cs
+++ Assets/Scripts/Object/PlayerController.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rigidbody;
+    private CircleCollider2D _collider;
 
     [SerializeField] private Vector2 _direction;
     [SerializeField] private float _force = 0;
@@ -12,10 +14,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _timeLimit = 10f;
     [SerializeField] private bool _isClick = false;
     [SerializeField] private bool _isGrounded = false;
+    [SerializeField] private UnityEvent<float> OnClickEvent;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CircleCollider2D>();
         _direction = _direction.normalized;
     }
 
@@ -33,7 +37,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (_isClick) _deltaTime += Time.deltaTime;
+        if (_isClick)
+        {
+            _deltaTime += Time.deltaTime;
+            OnClickEvent.Invoke(_deltaTime/_timeLimit);
+        }
+
+        if (!_isGrounded)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down * _collider.radius);
+            if (hit.collider != null) _isGrounded = true;
+        }
     }
 
     private void OnButtonDown(InputAction.CallbackContext context)
@@ -49,10 +63,5 @@ public class PlayerController : MonoBehaviour
         _deltaTime = 0;
         _isClick = false;
         _isGrounded = false;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        _isGrounded = true;
     }
 }
